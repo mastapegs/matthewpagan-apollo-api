6a0d8b401f7d7503d608f02c38b25f7d775765d5
Matthew Pagan <mastapegs01@gmail.com>
2020-11-15 00:28:52 -0500

    implemented first pagination

--- src/resolverFunctions/people.js
+++ src/resolverFunctions/people.js
@@ -1,23 +1,51 @@
-const people = require('../data/people')
+const peopleData = require('../data/people')
 
-const edges = people.map(({ name, isAdult }, cursor) => {
-  return ({
-    cursor,
-    node: {
-      name,
-      isAdult,
+const peopleConnection = (parent, { first, last, after, before }, context, info) => {
+
+  let edges
+  let PersonConnection
+  let hasNextPage
+
+  const edgesMapCallback = ({ name, isAdult }, cursor) => {
+    return ({
+      cursor,
+      node: {
+        name,
+        isAdult,
+      }
+    })
+  }
+
+  if (!first && !last) {
+
+    edges = peopleData.map(edgesMapCallback)
+    PersonConnection = {
+      edges,
+      pageInfo: {
+        hasPreviousPage: false,
+        hasNextPage: false,
+      },
     }
-  })
-})
 
-const peopleConnection = () => {
-  return ({
-    edges,
-    pageInfo: {
-      hasPreviousPage: false,
-      hasNextPage: false,
-    },
-  })
+  } else if (first) {
+
+    if (!after) {
+      edges = peopleData.slice(0, first).map(edgesMapCallback)
+      hasNextPage = first < peopleData.length
+      PersonConnection = {
+        edges,
+        pageInfo: {
+          hasPreviousPage: false,
+          hasNextPage,
+        },
+      }
+    }
+
+  }
+
+
+
+  return PersonConnection
 }
 
 module.exports = peopleConnection
--- src/typeDefs.js
+++ src/typeDefs.js
@@ -3,7 +3,7 @@ const { gql } = require('apollo-server')
 const typeDefs = gql`
 
   type Query {
-    people: PersonConnection
+    people(first: Int, last: Int, after: String, before: String): PersonConnection
     me: Summary
   }
 
