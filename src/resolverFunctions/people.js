fe71891b803064ae129560b152440c86ba59c70e
Matthew Pagan <mastapegs01@gmail.com>
2020-11-15 01:02:02 -0500

    implemented after

--- src/resolverFunctions/people.js
+++ src/resolverFunctions/people.js
@@ -2,11 +2,7 @@ const peopleData = require('../data/people')
 
 const peopleConnection = (parent, { first, last, after, before }, context, info) => {
 
-  let edges
-  let PersonConnection
-  let hasNextPage
-
-  const edgesMapCallback = ({ name, isAdult }, cursor) => {
+  const peopleEdgesMapCallback = ({ name, isAdult }, cursor) => {
     return ({
       cursor,
       node: {
@@ -15,10 +11,14 @@ const peopleConnection = (parent, { first, last, after, before }, context, info)
       }
     })
   }
+  
+  let edges = peopleData.map(peopleEdgesMapCallback)
+  let PersonConnection
+  let hasNextPage
 
   if (!first && !last) {
 
-    edges = peopleData.map(edgesMapCallback)
+    edges
     PersonConnection = {
       edges,
       pageInfo: {
@@ -30,8 +30,19 @@ const peopleConnection = (parent, { first, last, after, before }, context, info)
   } else if (first) {
 
     if (!after) {
-      edges = peopleData.slice(0, first).map(edgesMapCallback)
+      edges = edges.slice(0, first)
       hasNextPage = first < peopleData.length
+      PersonConnection = {
+        edges,
+        pageInfo: {
+          hasPreviousPage: false,
+          hasNextPage,
+        },
+      }
+    } else {
+      after = Number(after)
+      edges = edges.slice(after + 1, first + after + 1)
+      hasNextPage = (after + first < peopleData.length - 1) ? true : false
       PersonConnection = {
         edges,
         pageInfo: {
@@ -42,8 +53,6 @@ const peopleConnection = (parent, { first, last, after, before }, context, info)
     }
 
   }
-
-
 
   return PersonConnection
 }
