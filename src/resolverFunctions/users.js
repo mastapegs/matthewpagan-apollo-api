40b12795d2acd10d24355ef14c28274b8455df5d
Matthew Pagan <mastapegs01@gmail.com>
2020-11-18 23:55:29 -0500

    refactor

--- src/resolverFunctions/users.js
+++ src/resolverFunctions/users.js
@@ -1,7 +1,5 @@
 const users = async (_, { first, last, after, before }, { sequelize }) => {
-  const users = await sequelize.models.User.getAll({ first, last, after, before })
-  console.log(users[0])
-  return users
+  return await sequelize.models.User.getAll({ first, last, after, before })
 }
 
 module.exports = users
