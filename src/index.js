b4f09ead8834d2ccba77a61378d452dd9dddb166
Matthew Pagan <mastapegs01@gmail.com>
2020-11-18 22:26:49 -0500

    added sequlize connection and .env vars

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 require('dotenv').config()
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  dialect: 'mysql',
+});
+
+(async () => {
+
+  try {
+    await sequelize.authenticate();
+    console.log('Sequelize <=> MySQL Connection Established');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
+})()
 
 const { ApolloServer } = require('apollo-server')
 const typeDefs = require('./typeDefs')
