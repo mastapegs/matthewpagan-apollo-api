da4db4e85f9ef98469a41603a96671fa5e27f9af
Matthew Pagan <mastapegs01@gmail.com>
2020-11-21 15:17:42 -0500

    refactor typeDefs into .gql files

--- src/typeDefs/index.js
+++ src/typeDefs/index.js
@@ -1,10 +1,11 @@
 const fs = require('fs')
 const path = require('path')
+const { gql } = require('apollo-server')
 
 const typeDefs = fs.readdirSync(__dirname)
   .filter(file => file !== 'index.js')
   .map(file => {
-    return require(path.join(__dirname, file))
+    return gql`${fs.readFileSync(path.join(__dirname, file))}`
   })
 
 module.exports = typeDefs
