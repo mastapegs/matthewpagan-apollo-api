e2d0748817d11e7731ecb6191c9125b72596b55a
Matthew Pagan <mastapegs01@gmail.com>
2020-11-21 23:28:44 -0500

    effective login

--- src/models/User/login.js
+++ src/models/User/login.js
@@ -1,25 +1,9 @@
 const bcrypt = require('bcrypt')
 
 async function login({ email, password }) {
-
-  console.log(email)
-  console.log(password)
-  if (password) console.log((await bcrypt.hash(password, 10)))
-
   const checkUser = await this.findOne({ where: { email, } })
-  console.log(checkUser.password)
-
   const passwordMatch = await bcrypt.compare(password, checkUser.password)
-  console.log(passwordMatch)
-
-  return ({
-    id: '123',
-    email: 'sas',
-    password: '123123',
-    firstName: 'asdasd',
-    lastName: 'asdfasdf'
-  })
-
+  return (passwordMatch ? checkUser : { id: 'Failed to Authenticate' })
 }
 
 module.exports = login
