8144d0ea5d95bb694c5f0afba505c5723d91cb2a
Matthew Pagan <mastapegs01@gmail.com>
2020-11-21 22:59:38 -0500

    saving password correctly

--- src/models/User/register.js
+++ src/models/User/register.js
@@ -1,13 +1,23 @@
+const bcrypt = require('bcrypt')
+
 async function register(user) {
+
+  user.password = await bcrypt.hash(user.password, 10)
+
   const newUser = await this.create(user).catch(err => {
     if (err.name === 'SequelizeUniqueConstraintError') {
       return ({
         email: null,
         firstName: null,
+        password: null,
         lastName: null,
       })
     }
+    console.log(err)
   })
+
+  console.log(newUser)
+
   return ({
     ...newUser.dataValues,
     id: newUser.id || 'SequelizeUniqueConstraintError',
